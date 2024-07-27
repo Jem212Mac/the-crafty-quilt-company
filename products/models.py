e83b29692defae9249531df2274360021c3687a9
Jemima MacKenzie <jemimamackenzie@gmail.com>
2024-07-27 14:06:39 +0000

    feat: Add 'brand' field to product model

--- products/models.py
+++ products/models.py
@@ -21,6 +21,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     author = models.CharField(max_length=254, null=True, blank=True)
+    brand = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)

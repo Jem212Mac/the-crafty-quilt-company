8e445239abcd30efb1ddd9bc0e9b4d2a312303cd
Jemima MacKenzie <jemimamackenzie@gmail.com>
2024-07-27 17:47:32 +0000

    fix: Commit to push previously unsaved file

--- checkout/models.py
+++ checkout/models.py
@@ -44,7 +44,7 @@ class Order(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the order number
+        Override the default save method to set the order number
         if it hasn't been set already.
         """
         if not self.order_number:
@@ -63,7 +63,7 @@ class OrderLineItem(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the lineitem total
+        Override the default save method to set the lineitem total
         and update the order total.
         """
         self.lineitem_total = self.product.price * self.quantity

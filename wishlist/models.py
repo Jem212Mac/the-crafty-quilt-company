332f7f68c87c535ceafbeb7290c44f2b08887dd9
Jemima MacKenzie <jemimamackenzie@gmail.com>
2024-07-31 11:04:05 +0000

    feat: Create wishlist app and model and add wishlist button to product details

---
+++ wishlist/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from products.models import Product
+from profiles.models import User
+
+
+class Wishlist(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='wishlist'
+    )
+    products = models.ManyToManyField(
+        Product,
+        blank=True,
+        related_name='wishlist'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        editable=False
+    )

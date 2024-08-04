29eee960780bf0b5a93819c575f11c69c20aa98d
Jemima MacKenzie <jemimamackenzie@gmail.com>
2024-08-04 17:07:03 +0000

    feat: Create contact app, models & urls

---
+++ contact/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class contact(models.Model):
+    """
+    Stores a 'contact us' request message.
+    """
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Contact Us request from {self.name}"
---
+++ contact/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.contrib import messages
+from .models import contact
+
+# Create your views here.

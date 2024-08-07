9a0d341ed883678c076246e19de66c187032571a
Jemima MacKenzie <jemimamackenzie@gmail.com>
2024-08-07 18:41:47 +0000

    feat: Change debug setting to find server 500 issue

--- the_crafty_quilt_company/settings.py
+++ the_crafty_quilt_company/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = ['8000-jem212mac-thecraftyquil-c6n3gc4i2xf.ws.codeinstitute-ide.net','.herokuapp.com']
 

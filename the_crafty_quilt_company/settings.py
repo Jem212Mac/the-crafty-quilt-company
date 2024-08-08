040eac38500aa4d32cdb56ce328228625737481b
Jemima MacKenzie <jemimamackenzie@gmail.com>
2024-08-08 13:24:56 +0000

    feat: Move contact us to main nav and fix error caused by secret key being empty

--- the_crafty_quilt_company/settings.py
+++ the_crafty_quilt_company/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import dj_database_url
 
+if os.path.isfile('env.py'):
+    import env  # noqa
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.

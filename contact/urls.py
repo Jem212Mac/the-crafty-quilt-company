9c397a79810d4d7ac267cfce27d78a9485ff562d
Jemima MacKenzie <jemimamackenzie@gmail.com>
2024-08-12 19:05:58 +0000

    feat: Add / Amend comments for the ccontact app

--- contact/urls.py
+++ contact/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+""" urls for the contact form """
+
 urlpatterns = [
     path('', views.contact, name='contact'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.contrib import messages
 from .forms import ContactUs
 
-# Create your views here.
-
 
 def contact(request):
     if request.method == "POST":
